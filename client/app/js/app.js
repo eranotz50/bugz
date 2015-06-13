1d970bbdbf24fc3a74f48a2a37ece645abb57152
eranotz50 <eranotz50@gmail.com>
2015-06-13 20:48:36 +0000

    Login

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,22 +1,41 @@
+var bugzillaServerUrl =  "https://bugzilla.mozilla.org";
+
+
 var app = angular.module('app', [])
 
+
+.controller("LoginController",function($scope,$http){
+    console.log("LoginController");
+    
+    $scope.OnLoginClick = function (email,password){
+        
+          console.log(email);
+          console.log(password);
+        
+          var requestString = bugzillaServerUrl+"/rest/login?login="+ email + "&password=" + password ;
+        
+           $http.get(requestString)
+             .success(function(res){
+                console.log(res); 
+             }) ;
+    }
+    
+})
 
   
 .controller("OnLoadedController",function($scope,$http){
    console.log("loaded");
    
-   $scope.OnClick = function (){
-    console.log($http);
-    
-    var bugId = $scope.bugId;
-    console.log(bugId);
-    
-    $http.get("https://bugzilla.mozilla.org/rest/bug/"+bugId)
-        .success(function(data){
-            console.log(data);
+   $scope.OnClick = function (bugId){
+
+    console.log(bugId); 
+
+    $http.get(bugzillaServerUrl+"/rest/bug/"+bugId)
+        .success(function(res){
+            $scope.bug = res.bugs[0];
         });
    }
-}) 
+})
 
   
   
